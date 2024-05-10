aa86f74a08917ad338fdc1e08f9cd3de76d443e0
BitawareUnleashed <dario.benevento@gmail.com>
2024-05-11 00:50:29 +0200

    First commit

---
+++ Chapter9CQRS-API/Chapter9CQRS-API/Features/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using Chapter9CQRS_API.Features.Commands;
+using MediatR;
+
+namespace Chapter9CQRS_API.Features;
+
+public class CreateProductHandler 
+    : IRequestHandler<CreateProductCommand, int>
+{
+    public Task<int> Handle(
+        CreateProductCommand request
+        , CancellationToken cancellationToken)
+    {
+        // Here you would add your logic
+        // to save the product to the database
+        // For simplicity, let's assume
+        // we're returning a random ID
+        var newId = Random.Shared.Next(1, 10000);
+        return Task.FromResult(newId);
+    }
+}
+
